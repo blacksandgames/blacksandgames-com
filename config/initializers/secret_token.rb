a247044ebf77135264163a69cac4450f641cc377
Josh M <jmanuel@gapintelligence.com>
2014-01-17 17:14:08 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blacksandgames::Application.config.secret_key_base = '41295d1911e2be63f62c4d5ffa0d1aeef209389b50a8ec4924bdbb297d483ce5f58d4c6daeb2e1de999301fcd266486991f55c73bd09e69315f3f8ebfa7af90b'
