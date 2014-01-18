e8b9da28c88ca5518e1ac20f91e05d24e27e29a3
Josh Manuel <joshlatte@gmail.com>
2014-01-18 03:27:34 -0500

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140118004945) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "todos", force: true do |t|
     t.string   "name"
     t.boolean  "finished"
